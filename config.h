07a77030e8e75241f0446e5fa7a3dd0b3c8821ca
Sandy Ross <angstyduck69@gmail.com>
2020-10-04 12:44:13 +0100

    initial commit

---
+++ config.h
@@ -0,0 +1,51 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MOD Mod4Mask
+
+const char* menu[] 	= {"dmenu_run",					0};
+const char* term[] 	= {"st",					0};
+const char* scrot[] 	= {"scrot",					0};
+const char* mus[] 	= {"st", "-e", "ncmpcpp",			0};
+const char* web[] 	= {"firefox",					0};
+const char* briup[] 	= {"xbacklight", "-inc", "15",			0};
+const char* bridown[] 	= {"xbacklight", "-dec", "15",			0};
+const char* voldown[] 	= {"pamixer", "--allow-boost", "-d", "2",	0};
+const char* volup[]   	= {"pamixer", "--allow-boost", "-i", "2",	0};
+const char* volmute[] 	= {"pamixer", "-t",				0};
+
+static struct key keys[] = {
+    {MOD,      XK_q,   win_kill,   {0}},
+    {MOD,      XK_c,   win_center, {0}},
+    {MOD,      XK_f,   win_fs,     {0}},
+
+    {Mod1Mask,           XK_Tab, win_next,   {0}},
+    {Mod1Mask|ShiftMask, XK_Tab, win_prev,   {0}},
+
+    {MOD, XK_d,      run, {.com = menu}},
+    {MOD, XK_p,      run, {.com = scrot}},
+    {MOD, XK_m,      run, {.com = mus}},
+    {MOD, XK_w,      run, {.com = web}},
+    {MOD, XK_Return, run, {.com = term}},
+
+    {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
+    {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
+    {0,   XF86XK_AudioMute,         run, {.com = volmute}},
+    {0,   XF86XK_MonBrightnessUp,   run, {.com = briup}},
+    {0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},
+
+    {MOD,           XK_1, ws_go,     {.i = 1}},
+    {MOD|ShiftMask, XK_1, win_to_ws, {.i = 1}},
+    {MOD,           XK_2, ws_go,     {.i = 2}},
+    {MOD|ShiftMask, XK_2, win_to_ws, {.i = 2}},
+    {MOD,           XK_3, ws_go,     {.i = 3}},
+    {MOD|ShiftMask, XK_3, win_to_ws, {.i = 3}},
+    {MOD,           XK_4, ws_go,     {.i = 4}},
+    {MOD|ShiftMask, XK_4, win_to_ws, {.i = 4}},
+    {MOD,           XK_5, ws_go,     {.i = 5}},
+    {MOD|ShiftMask, XK_5, win_to_ws, {.i = 5}},
+    {MOD,           XK_6, ws_go,     {.i = 6}},
+    {MOD|ShiftMask, XK_6, win_to_ws, {.i = 6}},
+};
+
+#endif
